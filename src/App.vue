fe31787362c0e0b0c118f6f179c94dd41cbdf6a3
huasi <1431836316@qq.com>
2023-04-07 22:39:18 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>{{$store.state.name}}</h2>
+    <router-link to="/login">登录</router-link>
+    <router-link to="/main">主页</router-link>
+    <router-view></router-view>
+    <el-button>默认按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+  </div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  }
+})
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
